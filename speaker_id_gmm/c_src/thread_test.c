a269e70cf39134c09e62e6edbbab86f1634743ce
chenguangshen <pscggeforce@gmail.com>
2013-06-12 01:21:55 -0700

    update multithread compare result

--- speaker_id_gmm/c_src/thread_test.c
+++ speaker_id_gmm/c_src/thread_test.c
@@ -13,68 +13,72 @@
 #include <hexagon_standalone.h>
 #include "hexagon_sim_timer.h"
 
-#define COMPUTE_THREADS 2
-#define STACK_SIZE 16384
+#define COMPUTE_THREADS 1
+// might be the minimum stack size for each thread
+#define STACK_SIZE 3192
 
 #define FORMAT "%lf\n"
 #define PREC "%.3f\n"
 #define NR_SPEAKER 5
 #define SAMPLE_SIZE 167
+#define SCALE 1000.0
 
 typedef double real_T;
+typedef double Word16;
 
 typedef struct by_speaker {
     char name[10];
-    real_T m_m[108];
-    real_T m_v[108];
-    real_T m_w[9];
+    Word16 m_m[108];
+    Word16 m_v[108];
+    Word16 m_w[9];
 } by_speaker;
 
 static const int num_mixture = 9;
 static const int num_speaker = NR_SPEAKER;
 static by_speaker speaker_gmm[NR_SPEAKER];
-static real_T test_sample[SAMPLE_SIZE][12];
+static Word16 test_sample[SAMPLE_SIZE][12];
 static char ground_truth[10];
 static char filename[100];
 static char str[5];
 static real_T logprob[NR_SPEAKER];
 static real_T min, max;
 
-static void compute_fractal (int * quadrant, int i);
+static void compute (int * speaker_id, int i);
 static int Mx;
 static char stack [COMPUTE_THREADS][STACK_SIZE] __attribute__ ((__aligned__(8))); ;
-static int quadrants [5] = {0,1,2,3,4};
+static int speaker_ids [5] = {0,1,2,3,4};
 
 
-double gaussmixp(real_T x[12], real_T mu[108], real_T sigma[108], real_T w[9]) {
+float gaussmixp(Word16 x[12], Word16 mu[108], Word16 sigma[108], Word16 w[9]) {
     int i = 0, j = 0;
     real_T score = 0;
     for (i = 0; i < 9; i++) {
         real_T y = 0;
         for (j = 0; j < 12; j++) {
-            real_T m = mu[j * 9 + i];
+            Word16 m = mu[j * 9 + i];
             real_T s = 1.0 / sigma[j * 9 + i];
             //printf("%d %d %lf %lf\n", j, i, m, s);
-            y += (x[j] - m) * (x[j] - m) * s;
+            y += (x[j] - m) * (x[j] - m) * s ;
         }
         score += w[i] * (1.0/pow(6.828 ,6)) * (1.0 / pow(1.0,0.5)) * exp(-0.5 * y);
     }
     return log(score);
 }
 
-real_T round_real(real_T x) {
+Word16 round_real(real_T x) {
     if (x > max) {
         max = x;
     }
     if (x < min) {
         min = x;
     }
-    real_T result = 0;
-    char buffer[128];
-    memset(buffer, 0, sizeof(buffer));
-    sprintf(buffer, PREC, x);
-    sscanf(buffer, FORMAT, &result);
-    return result;
+    return x;
+    // real_T result = 0;
+    // char buffer[128];
+    // memset(buffer, 0, sizeof(buffer));
+    // sprintf(buffer, PREC, x);
+    // sscanf(buffer, FORMAT, &result);
+    // return result;
 }
 
 int get_label(const char *name) {
@@ -124,8 +128,9 @@ void load_speaker_gmm () {
             //printf("read M!\n");
             int j = 0;
             for (j = 0; j < 108; j++) {
-                fscanf(fin, FORMAT, &speaker_gmm[i].m_m[j]);
-                //speaker_gmm[i].m_m[j] = round_real(speaker_gmm[i].m_m[j]);
+                real_T temp;
+                fscanf(fin, FORMAT, &temp);
+                speaker_gmm[i].m_m[j] = round_real(temp);
             }
             // for (j = 0; j < 108; j++) {
             //  printf("%d: %lf\n", j, speaker_gmm[i].m_m[j]);
@@ -139,8 +144,9 @@ void load_speaker_gmm () {
             //printf("read V!\n");
             int j = 0;
             for (j = 0; j < 108; j++) {
-                fscanf(fin, FORMAT, &speaker_gmm[i].m_v[j]);
-                //speaker_gmm[i].m_v[j] = round_real(speaker_gmm[i].m_v[j]);
+                real_T temp;
+                fscanf(fin, FORMAT, &temp);
+                speaker_gmm[i].m_v[j] = round_real(temp);
             }
             // for (j = 0; j < 108; j++) {
             //  printf("%d: %lf\n", j, speaker_gmm[i].m_v[j]);
@@ -154,8 +160,9 @@ void load_speaker_gmm () {
             //printf("read W!\n");
             int j = 0;
             for (j = 0; j < 9; j++) {
-                fscanf(fin, FORMAT, &speaker_gmm[i].m_w[j]);
-                //speaker_gmm[i].m_w[j] = round_real(speaker_gmm[i].m_w[j]);
+                real_T temp;
+                fscanf(fin, FORMAT, &temp);
+                speaker_gmm[i].m_w[j] = round_real(temp);
             }
             // for (j = 0; j < 9; j++) {
             //  printf("%d: %lf\n", j, speaker_gmm[i].m_w[j]);
@@ -185,8 +192,9 @@ int load_test_sample (const char *file_name) {
         int i = 0, j = 0;
         for (i = 0; i < n; i++) {
             for (j = 0; j < 12; j++) {
-                fscanf(fin, FORMAT, &test_sample[i][j]);
-                //test_sample[i][j] = round_real(test_sample[i][j]);
+                real_T temp;
+                fscanf(fin, FORMAT, &temp);
+                test_sample[i][j] = round_real(temp);
             }
         }
         fclose(fin);
@@ -200,8 +208,7 @@ int load_test_sample (const char *file_name) {
 }
 
 
-int main (int argc, char **argv)
-{
+int main (int argc, char **argv){
     load_speaker_gmm();
     int k = 1, i = 0, j = 0;
     int correct = 0;
@@ -230,7 +237,7 @@ int main (int argc, char **argv)
     j = 0;
     while (j < num_speaker) {
         for (i = 0; i < COMPUTE_THREADS && j < num_speaker; i++, j++) {
-            thread_create ((void *) compute_fractal, &stack [i][STACK_SIZE], i + 1, quadrants + j);
+            thread_create ((void *) compute, &stack [i][STACK_SIZE], i + 1, speaker_ids + j);
             printf("create thread %d for j=%d\n", (i+1), j);
         }
         thread_join (((1 << COMPUTE_THREADS) - 1) << 1);
@@ -248,16 +255,17 @@ int main (int argc, char **argv)
         }
     }
     printf("speaker label = %d\n", (max_index + 1));
+    printf("max=%f\nmin=%f\n", max, min);
 
 
-    // Create threads to compute four quadrants of the fractal
+    // Create threads to compute four speaker_ids of the fractal
     // each thread is allocated a stack base and parameter
 
     // j = 0;
     // while (j < 10)
     // {
     //     for (i = 0; i < COMPUTE_THREADS && j < 10; i++, j++) {
-    //         thread_create ((void *) compute_fractal, &stack [i][STACK_SIZE], i + 1, quadrants + j);
+    //         thread_create ((void *) compute_fractal, &stack [i][STACK_SIZE], i + 1, speaker_ids + j);
     //         printf("create thread %d for j=%d\n", (i+1), j);
     //     }
     //     //thread_join (((1 << COMPUTE_THREADS) - 1) << 1);
@@ -270,20 +278,20 @@ int main (int argc, char **argv)
     return (0);
 }
 
-static void compute_fractal (int * quadrant, int i)
+static void compute (int * speaker_id, int i)
 {
     while (1)
     {
         if (trylockMutex (&Mx))
         {
-            printf("thread %d for: %d\n", thread_get_tnum(), *quadrant);
+            printf("thread %d for: %d\n", thread_get_tnum(), *speaker_id);
             break;
         }
 
     }
     unlockMutex (&Mx);
 
-    int j = *quadrant;
+    int j = *speaker_id;
     by_speaker gmm = speaker_gmm[j];
     int iii = 0;
     for (iii = 0; iii < 167; iii++) {
@@ -293,7 +301,15 @@ static void compute_fractal (int * quadrant, int i)
 
     lockMutex (&Mx);
     {
-        printf("\nDone computing fractal for thread %d.\n", thread_get_tnum());
+        printf("\nDone computing gmm prob for thread %d.\n", thread_get_tnum());
     }
     unlockMutex (&Mx);
 }
+
+// 1 thread, stack size=3192
+// use double: 22574464 cycles, 94136B memory
+// use float: 5326153 cycles, 81576B memory
+
+// 2 threads, stack size=3192 
+// use double: 14817336 cycles, 97536B memory
+// use float: 3449244 cycles, 84800B memory
